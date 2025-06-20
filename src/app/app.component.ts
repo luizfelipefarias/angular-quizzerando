777c1b944ee518ca47bb65c3a7eb92d5da1ea9a4
Pedro H. Silva <impedrohenri@gmail.com>
2025-06-20 20:16:06 -0300

    remove componentes inúteis, instala angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MatToolbarModule, MatIconModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quizzerando';
+  isAuthenticated: boolean = true;
 }
