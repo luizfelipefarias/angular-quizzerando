c2c5ea0ba8b1447c860b72f89516454ab2d152d6
Pedro H. Silva <impedrohenri@gmail.com>
2025-06-27 00:47:38 -0300

    adiciona página de edição de quiz

--- src/pages/editar-quiz/editar-quiz.component.ts
+++ src/pages/editar-quiz/editar-quiz.component.ts
@@ -1,26 +1,75 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { ModalComponent } from '../../components/modal/modal.component';
+import { FormsModule } from '@angular/forms';
+import { Pergunta, Quiz, QuizzesService } from '../../app/services/quizzes.service';
+import CategoriasIcons from '../../../src/assets/categoriasIcons.json'
 
 @Component({
   selector: 'app-editar-quiz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ModalComponent, FormsModule],
   templateUrl: './editar-quiz.component.html',
   styleUrl: './editar-quiz.component.css'
 })
 export class EditarQuizComponent {
-  public quizId : string | null = '';
-  public nomeQuiz : string = '';
+  protected quizId : string | null = '';
+  protected quiz!: Quiz;
+  protected perguntas!: Pergunta[];
 
-  constructor(private route: ActivatedRoute, private serviceTitle: Title){
+  protected validated: boolean = false;
+  protected altsIncorretas: string[] = [];
+  protected categorias = CategoriasIcons;
+
+  modelo: any = {
+    enunciado: '',
+    respCorreta: '',
+    alternativa1: '',
+    alternativa2: '',
+    alternativa3: '',
+    alternativa4: ''
+  };
+
+
+  constructor(private route: ActivatedRoute, private serviceTitle: Title, private serviceQuiz: QuizzesService){
     this.serviceTitle.setTitle('Editando - ');
   }
 
   ngOnInit(){
     this.quizId = this.route.snapshot.paramMap.get('id');
-    this.serviceTitle.setTitle('Editando - ' + this.nomeQuiz);
+    
+    this.serviceQuiz.getQuizById(this.quizId).subscribe((data) =>{
+      this.quiz = data;
+      this.serviceTitle.setTitle('Editando - ' + this.quiz.titulo);
+    })
+
+    this.serviceQuiz.getPerguntasByQuizId(this.quizId).subscribe((data) =>{
+      this.perguntas = data;
+    })
   }
 
+  handleAddIconrretas() {
+    this.altsIncorretas.push('')
+  }
 
+  removeAltIncorreta(index: number) {
+    this.altsIncorretas = this.altsIncorretas.filter((_, i) => i !== index)
+  }
+
+  handleCadastroPerguntas(formData: any) {
+    console.log("2", formData.value)
+    this.perguntas.push(formData.value)
+    console.log(this.perguntas)
+    return false;
+  }
+
+  handleDelete(index: number){
+    this.perguntas.splice(index, 1);
+  }
+
+  getCategoriasKeys(): string[] {
+  return Object.keys(this.categorias);
+  }
 }
